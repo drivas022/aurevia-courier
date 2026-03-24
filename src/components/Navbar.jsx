4b6b84398b7fb3be1ae2bf96d79751348b269ee2
Diego Rivas <drivas022@yahoo.com>
2026-03-23 19:37:17 -0600

    fix: AreoPAQ name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
       <div className="navbar__contenedor">
         {/* Logo */}
         <Link to="/" className="navbar__logo">
-          AUREVIA
+          AeroPAQ
         </Link>
 
         {/* Menú */}
